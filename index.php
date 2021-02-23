a6f3a362e6d6775242384cda98688477ee36e259
Favour <toluwaniokunowo@gmail.com>
2021-02-23 09:26:15 +0100

    Task Complete

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
+<?php
+  $a; $b;
 
+  function calculate ($a, $b) {
+    echo $a + $b . '</br>';
+    echo $a - $b . '</br>';
+    echo $a * $b . '</br>';
+    echo $a / $b . '</br>';
+  }
+
+  echo '<h1>Simple Calculator</h1>';
+  calculate(20, 5);
+?>
